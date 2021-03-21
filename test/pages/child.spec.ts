71f47612eec15db36d9856488827f3cb6d6eac69
tak001 <twitwitwi2@yahoo.co.jp>
2021-03-21 11:22:26 +0900

    change:ts

--- test/pages/child.spec.ts
+++ test/pages/child.spec.ts
@@ -1,11 +1,16 @@
-import { createLocalVue, mount, RouterLinkStub } from '@vue/test-utils';
-import ChildPage from '~/pages/child';
+import {
+  createLocalVue,
+  mount,
+  RouterLinkStub,
+  Wrapper,
+} from '@vue/test-utils';
+import ChildPage from '~/pages/child.vue';
 
 const localVue = createLocalVue();
 
 describe('pages/child.vue', () => {
   test('トップページへ戻る導線が存在する', () => {
-    const wrapper = mount(ChildPage, {
+    const wrapper: Wrapper<any> = mount(ChildPage, {
       localVue,
       stubs: {
         NuxtLink: RouterLinkStub,
